e322e09e1e6a8d3ef5e75d9c94f85fe2b248ae28
Prabir Rout <prabirrout32@gmail.com>
2025-05-06 13:19:43 +0530

    Update MiniStatement.java

--- Bank Management System/src/bank/management/system/MiniStatement.java
+++ Bank Management System/src/bank/management/system/MiniStatement.java
@@ -1,10 +1,8 @@
-
 package bank.management.system;
 
 import java.awt.Color;
 import javax.swing.*;
 import java.sql.*;
-
 
 public class MiniStatement  extends JFrame{
 
@@ -22,8 +20,7 @@ public class MiniStatement  extends JFrame{
         JLabel mini = new JLabel();
         mini.setBounds(20,140,400,200);
         add(mini);
-        
-        
+                
         JLabel bank = new JLabel("SBI Bank");
         bank.setBounds(130,20,100,20);
         add(bank);
@@ -41,19 +38,16 @@ public class MiniStatement  extends JFrame{
             ResultSet rs = conn.s.executeQuery("select * from login where pin = '"+pinno+"'");
             while(rs.next()){
                 card.setText("Card Number: "+rs.getString("cardNumber").substring(0, 4)+"XXXXXXXX"+rs.getString("cardnumber").substring(12));
-            }
-            
+            }   
         }catch(Exception e){
             System.out.println(e);
         }
-        
         try{
             Conn conn = new Conn();
              int bal = 0;
             ResultSet rs = conn.s.executeQuery("select * from bank where pin = '"+pinno+"'");
             while(rs.next()){
                mini.setText(mini.getText() + "<html>" +rs.getString("date") + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + rs.getString("type")+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+ rs.getString("amount")+"<br><br></html>");
-        //  mini.setText("Sc asd sad49 9a8sdv9 91asd");
         if(rs.getString("type").equals("Deposit")){
                         bal += Integer.parseInt(rs.getString("amount"));
                     }else{
@@ -64,15 +58,12 @@ public class MiniStatement  extends JFrame{
             }
             catch(Exception e){
             System.out.println(e);
-        }
-        
-        
+        }  
         setSize(400,600);
         getContentPane().setBackground(Color.WHITE);
         setVisible(true);
         setLocation(20,20);
     }
-    
     public static void main(String args[]) {
         new MiniStatement("");
     }
